2f3985915a3cd584b04fe78c0ca635d186aca7d7
Bahri Meriç CANLI <bahri@bahri.info>
2015-07-04 11:11:30 +0300

    biraz daha rutuş

--- js/script.js
+++ js/script.js
@@ -156,3 +156,66 @@ $(document).ready(function(){
     $("#page1 #mainButton").hide();
 
 });
+
+
+
+function jqemoji(str){
+
+    var haut = 0;
+    var n = 0;
+    CPstring = '';
+
+    for(i=0;i<str.length;i++){
+        var code = str.charCodeAt(i);
+        var b = str.charCodeAt(i); 
+
+        if(code>1000) {
+
+            if (b < 0 || b > 0xFFFF) {
+                CPstring += 'Error ' + dhex(b).toLowerCase() + '!';
+            }
+            if (haut != 0) {
+                if (0xDC00 <= b && b <= 0xDFFF) {
+                    CPstring += ' imgFront'+ dhex(0x10000 + ((haut - 0xD800) << 10) + (b - 0xDC00)).toLowerCase() + 'imgBack ';
+                    haut = 0;
+                    continue;
+                    }
+                else {
+                    CPstring += '!erreur ' + dhex(haut).toLowerCase() + '!';
+                    haut = 0;
+                    }
+            }
+
+            if (0xD800 <= b && b <= 0xDBFF) {
+                haut = b;
+            }
+            else {
+                CPstring += ' imgFront'+ dhex(b).toLowerCase() + 'imgBack ';
+            }
+
+        }
+        else {
+                CPstring = CPstring+String.fromCharCode(code);
+        }
+            
+        
+    }
+
+    CPstring = CPstring.substring(0, CPstring.length-1);
+    CPstring = replaceAll(CPstring , 'imgFront', '<img class="emoji-img" width="16" src="http://twemoji.maxcdn.com/16x16/');
+    CPstring = replaceAll(CPstring, 'imgBack', '.png"/>');
+
+    return CPstring;
+};
+
+function dhex(str) {
+ return (str+0).toString(16).toUpperCase();
+}
+
+function escapeRegExp(string) {
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(string, find, replace) {
+  return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
