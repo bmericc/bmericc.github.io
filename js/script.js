8badeb93138a7e8793ff7bf164bf0ff415872e99
Bahri Meriç CANLI <bahri@bahri.info>
2016-09-11 21:13:58 +0300

    panel padding

--- js/script.js
+++ js/script.js
@@ -112,6 +112,7 @@ if( getVersion()!=0 ) {
         $(".ui-header").css( {"padding-top" : "20px", "height":"45px"} );
         $(".ui-header > a.ui-link").css( {"margin-top":"25px"} );
         $(".ui-title").css( {"margin-top" : "10px"} );
+        $(".ui-panel").css( {"padding-top" : "20px"} );        
      }, 150); 
 }
 
@@ -121,6 +122,7 @@ $( document ).on( "pagebeforeshow", function() {
         $(".ui-header").css( {"padding-top" : "20px", "height":"45px"} );
         $(".ui-header > a.ui-link").css( {"margin-top":"25px"} );
         $(".ui-title").css( {"margin-top" : "10px"} );
+        $(".ui-panel").css( {"padding-top" : "20px"} );                
     }
 
 });
