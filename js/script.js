5cea2fa549715cb9b705be503564213f5af93c5a
Bahri Meriç Canlı <bahri@bahri.info>
2018-07-30 10:06:22 +0300

    function

--- js/script.js
+++ js/script.js
@@ -242,6 +242,18 @@ function replaceAll(string, find, replace) {
   return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
+
+function getQueryVariable(variable)
+{
+   var query = window.location.search.substring(1);
+   var vars = query.split("&");
+   for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+   }
+   return(false);
+}
+
 $(document).on("pagebeforeshow", "#contentPage", function () {
 
     var tag = getQueryVariable("tag");
